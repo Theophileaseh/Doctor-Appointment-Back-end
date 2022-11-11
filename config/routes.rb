e3c051eb85a2e1886dbb4ed4c8fe3ad63f556bad
petushka1 <nadya@devery.io>
2022-11-11 12:31:59 +0200

    add Doctor actions

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :users, only: :index
+      resources :doctors, only: [:index, :show, :new, :create]
     end
   end
 end
