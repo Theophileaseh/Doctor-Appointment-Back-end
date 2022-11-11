76c0b2f30842703cd4686a4bd62b3d5c7d4565cb
petushka1 <nadya@devery.io>
2022-11-11 11:55:03 +0200

    create doctors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_11_090324) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_11_095352) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.string "specialty"
+    t.string "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
