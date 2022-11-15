c9d3647059848c70a1c74395d67a843d130b85e8
Theophileaseh <theophilenwachan@gmail.com>
2022-11-15 14:20:36 +0100

    Create swagger rspec test templates for controllers

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,5 +1,6 @@
 class Doctor < ApplicationRecord
   has_many :appointments
+  belongs_to :user
 
   validates :name, presence: true
   validates :photo, presence: true
--- db/migrate/20221115131332_add_user_id_to_doctors.rb
+++ db/migrate/20221115131332_add_user_id_to_doctors.rb
@@ -1,6 +1,5 @@
 class AddUserIdToDoctors < ActiveRecord::Migration[7.0]
   def change
-    add_column :doctors, :user_id, :integer
     add_reference :doctors, :user, null: false, foreign_key: true
   end
 end
