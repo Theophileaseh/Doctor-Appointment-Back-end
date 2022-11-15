ab71ba5040aaa6ccdf01dacefc91833fe82f0c79
petushka1 <nadya@devery.io>
2022-11-16 01:27:14 +0200

    add tests for appointment and doctor

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,7 +2,7 @@ class Doctor < ApplicationRecord
   has_many :appointments
   belongs_to :user
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3 }
   validates :photo, presence: true
   validates :specialty, presence: true
 end
--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Doctor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe('associations') do
+    it { should belong_to(:user) }
+    it { should have_many(:appointments) }
+  end
+
+  describe 'should be invalid' do
+   it 'can\'t be created without arguments' do
+    doctor = Doctor.create()
+    expect(doctor).to_not be_valid
+   end
+
+   it 'can\'t be created without arguments' do
+    doctor = Doctor.create()
+    expect(doctor).to_not be_valid
+   end
+
+   it 'expect name to not be less than 3 characters length' do
+    doctor = Doctor.create(name: 'as', photo: 'img.test.com', specialty: 'some', bio: 'test bio')
+    expect(doctor).to_not be_valid
+   end
+  end
 end
