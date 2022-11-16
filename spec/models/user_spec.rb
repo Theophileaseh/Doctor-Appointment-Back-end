682213b0a053a48fadf4ab1407eec65d8006bbb5
petushka1 <nadya@devery.io>
2022-11-16 10:10:16 +0200

    add more test cases for User Model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User, type: :model do
       email: 'test@mail.com',
       password: 'hgdu47sk',
       name: 'Superman',
-      birth_date: '04-05-2000',
+      birth_date: '2000-05-04',
       gender: 'f'
     )
   end
@@ -19,10 +19,49 @@ RSpec.describe User, type: :model do
     it 'gender to not be "m"' do
       expect(@user.gender).to_not eq('m')
     end
+
+    it 'birth_day to be "04-05-2000"' do
+      expect(@user.birth_date.to_s).to eq('2000-05-04')
+    end
+
+    it 'email to be "test@mail.com"' do
+      expect(@user.email).to eq('test@mail.com')
+    end
+
+    it 'password to be "hgdu47sk"' do
+      expect(@user.password).to eq('hgdu47sk')
+    end
   end
 
+  # Should correspond associations set
   describe('associations') do
     it { should have_many(:appointments) }
     it { should have_many(:doctors) }
+  end
+
+  describe 'Should not be valid' do
+    it 'Should contain arguments' do
+      expect(User.create).to_not be_valid
+    end
+
+    it 'Name should be at least 3 characters long' do
+      expect(User.create(
+               email: 'test@mail.com',
+               password: 'hgdu47sk',
+               name: 'S',
+               birth_date: '2000-05-04',
+               gender: 'f'
+             )).to_not be_valid
+    end
+
+    it 'Email should be of proper format' do
+      expect(User.create(
+               email: 'my email',
+               password: 'hgdu47sk',
+               name: 'S',
+               birth_date: '2000-05-04',
+               gender: 'f'
+             )).to_not be_valid
+    end
   end
 end
