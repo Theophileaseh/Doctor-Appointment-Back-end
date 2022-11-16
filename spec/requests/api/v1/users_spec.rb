b60f0bf8f6dc0f0eac49f7a1f435f3e177c45116
Theophileaseh <theophilenwachan@gmail.com>
2022-11-16 17:18:12 +0100

    Add users request test

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -16,7 +16,28 @@ RSpec.describe 'api/v1/users', type: :request do
     end
 
     post('create user') do
-      response(200, 'successful') do
+      response(422, 'successful') do
+        consumes 'application/json'
+
+        parameter name: :user, in: :body, schema: {
+          type: :object,
+          properties: {
+            name: { type: :string },
+            email: { type: :string },
+            role: { type: :string },
+            birth_date: { type: :string },
+            gender: { type: :string },
+            password: { type: :string },
+            password_confirmation: { type: :string }
+          },
+          required: %w[name email birth_date gender password password_confirmation role]
+        }
+        let(:user) do
+          User.create(name: 'Nwachan', email: "nwachan@gmail.com", birth_date: { type: :string },
+            gender: { type: :string }, password: '123456',
+                      password_confirmation: '123456', role: 'admin')
+        end
+
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -35,7 +56,16 @@ RSpec.describe 'api/v1/users', type: :request do
 
     get('show user') do
       response(200, 'successful') do
-        let(:id) { '123' }
+        let(:user) do
+          User.create(name: 'Nwachan', email: "nwachan@gmail.com", birth_date: { type: :string },
+            gender: { type: :string }, password: '123456',
+                      password_confirmation: '123456', role: 'admin')
+        end
+        let(:id) do
+          User.create(name: 'Nwachan', email: "nwachan@gmail.com", birth_date: { type: :string },
+            gender: { type: :string }, password: '123456',
+                      password_confirmation: '123456', role: 'admin').id
+        end
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -48,9 +78,29 @@ RSpec.describe 'api/v1/users', type: :request do
       end
     end
 
-    delete('delete user') do
+    patch('update user') do
       response(200, 'successful') do
-        let(:id) { '123' }
+        let(:user) do
+          User.create(name: 'Nwachan', email: "nwachan@gmail.com", birth_date: { type: :string },
+          gender: { type: :string }, password: '123456',
+                      password_confirmation: '123456', role: 'admin')
+        end
+        let(:id) { User.all.last.id }
+        consumes 'application/json'
+
+        parameter name: :user, in: :body, schema: {
+          type: :object,
+          properties: {
+            name: { type: :string },
+            email: { type: :string },
+            role: { type: :string },
+            birth_date: { type: :string },
+            gender: { type: :string },
+            password: { type: :string },
+            password_confirmation: { type: :string }
+          },
+          required: %w[name email birth_date gender password password_confirmation role]
+        }
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -59,8 +109,83 @@ RSpec.describe 'api/v1/users', type: :request do
             }
           }
         end
-        run_test!
       end
+    end
+
+    put('update user') do
+      response(200, 'successful') do
+        let(:user) do
+          User.create(name: 'Nwachan', email: "nwachan@gmail.com", birth_date: "01/01/2005", gender: "M", password: '123456',
+                      password_confirmation: '123456', role: 'admin')
+        end
+        let(:id) do
+          User.create(name: 'Nwachan', email: "nwachan@gmail.com", birth_date: "01/01/2005", gender: "M", password: '123456',
+                      password_confirmation: '123456', role: 'admin').id
+        end
+        consumes 'application/json'
+
+        parameter name: :user, in: :body, schema: {
+          type: :object,
+          properties: {
+            name: { type: :string },
+            email: { type: :string },
+            role: { type: :string },
+            birth_date: { type: :string },
+            gender: { type: :string },
+            password: { type: :string },
+            password_confirmation: { type: :string }
+          },
+          required: %w[name email birth_date gender password password_confirmation role]
+        }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+      end
+    end
+
+    delete('delete user') do
+      response(200, 'successful') do
+        let(:id) { User.all.last.id }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+      end
+    end
+  end
+
+  path '/login' do
+    post('login user') do
+      response(200, 'successful') do
+        consumes 'application/json'
+
+        parameter name: :user, in: :body, schema: {
+          type: :object,
+          properties: {
+            email: { type: :string },
+            password: { type: :string }
+          },
+          required: %w[email password]
+        }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+      end
+    end
     end
   end
 end
