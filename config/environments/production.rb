0654ae93f77845c7300dd6e94ea20fe0eb1fa565
Theophileaseh <theophilenwachan@gmail.com>
2022-11-16 14:34:11 +0100

    Update auth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,7 @@ Rails.application.configure do
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -53,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "Doctor_Appointment_Back_end_production"
+  # config.active_job.queue_name_prefix = "Doctor_appointment_backend_production"
 
   config.action_mailer.perform_caching = false
 
