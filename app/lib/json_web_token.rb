a06a0a26086b1d3fe8cc2c775a901bb4459563ae
Theophileaseh <theophilenwachan@gmail.com>
2022-11-16 11:37:34 +0100

    add jwt

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,7 +1,8 @@
+require 'jwt'
 class JsonWebToken
-  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+  SECRET_KEY = Rails.application.secrets.secret_key_base
 
-  def self.encode(payload, exp = 24.hours.from_now)
+  def self.encode(payload, exp = 1200.hours.from_now)
     payload[:exp] = exp.to_i
     JWT.encode(payload, SECRET_KEY)
   end
