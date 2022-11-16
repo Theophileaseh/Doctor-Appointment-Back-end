0944a206daf10932b7436b6f7dbc073124967dfa
petushka1 <nadya@devery.io>
2022-11-16 09:46:18 +0200

    user models test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:all) do
+    @user = User.create(
+      email: 'test@mail.com',
+      password: 'hgdu47sk',
+      name: 'Superman', 
+      birth_date: '04-05-2000',
+      gender: 'f'
+    )
+  end
+
+  describe 'Should be created with valid arguments' do
+    it 'name to equal "Superman"' do
+      expect(@user.name).to eq('Superman')
+    end
+
+    it 'gender to not be "m"' do
+      expect(@user.gender).to_not eq('m')
+    end
+  end
+
+
+  describe('associations') do
+    it { should have_many(:appointments) }
+    it { should have_many(:doctors) }
+  end
 end
